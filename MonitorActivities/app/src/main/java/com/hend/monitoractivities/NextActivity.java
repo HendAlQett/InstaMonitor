b6c6d3f90c8dd4eed0bfd4c5a8a1870268d3c8f9
Hend <cathe.2007@gmail.com>
2016-06-02 16:00:08 +0200

    Remove unused code

--- MonitorActivities/app/src/main/java/com/hend/monitoractivities/NextActivity.java
+++ MonitorActivities/app/src/main/java/com/hend/monitoractivities/NextActivity.java
@@ -2,15 +2,7 @@ package com.hend.monitoractivities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.TextView;
-
-import com.hend.instamonitor.Monitor;
-
-import java.util.Collections;
-import java.util.List;
 
 import butterknife.ButterKnife;
 
@@ -18,43 +10,17 @@ import butterknife.ButterKnife;
 public class NextActivity extends AppCompatActivity {
 
 
-    TextView textView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_next);
         ButterKnife.bind(this);
-
-//        textView = (TextView) findViewById(R.id.textView);
-
-//        long time = Monitor.getInstance().getTimeInSeconds(getApplicationContext(), Monitor.APPLICATION_KEY);
-//
-//        textView.setText("Main: " + time);
-
-
-    }
-
-    public List<Fragment> getVisibleFragments() {
-        List<Fragment> allFragments = getSupportFragmentManager().getFragments();
-        if (allFragments == null || allFragments.isEmpty()) {
-            return Collections.emptyList();
-        }
-        else
-           return allFragments;
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        //TODO check list no empty
-        String fragmentSimpleName=null;
-        if (getVisibleFragments().get(0)!=null) {
-             fragmentSimpleName = getVisibleFragments().get(0).getClass().getSimpleName();
-            Log.d("Monitor", "Simple Name: " + getVisibleFragments().get(0).getClass().getSimpleName());
-        }
-//        long time = Monitor.getInstance().getTimeInSeconds(getApplicationContext(),fragmentSimpleName);
-        long time = Monitor.getInstance().getTimeInSeconds(getApplicationContext(), MainActivity.class.getSimpleName());
-//        textView.setText("Main: " + time);
+
     }
 }
