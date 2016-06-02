aef4e0e4b3865e0d9f0773efb59108bb61ba3b47
Hend <cathe.2007@gmail.com>
2016-06-02 16:01:21 +0200

    Model implements parcelable

--- MonitorActivities/app/src/main/java/com/hend/monitoractivities/models/ActivityMonitored.java
+++ MonitorActivities/app/src/main/java/com/hend/monitoractivities/models/ActivityMonitored.java
@@ -1,13 +1,18 @@
 package com.hend.monitoractivities.models;
 
 
-public class ActivityMonitored {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class ActivityMonitored implements Parcelable{
     boolean isMonitored =true;
     String activitySimpleName;
 
     public ActivityMonitored(String activitySimpleName) {
         this.activitySimpleName = activitySimpleName;
     }
+
+
 
     public boolean isMonitored() {
         return isMonitored;
@@ -23,5 +28,33 @@ public class ActivityMonitored {
 
     public void setActivitySimpleName(String activitySimpleName) {
         this.activitySimpleName = activitySimpleName;
+    }
+
+
+    protected ActivityMonitored(Parcel in) {
+        isMonitored = in.readByte() != 0;
+        activitySimpleName = in.readString();
+    }
+
+    public static final Creator<ActivityMonitored> CREATOR = new Creator<ActivityMonitored>() {
+        @Override
+        public ActivityMonitored createFromParcel(Parcel in) {
+            return new ActivityMonitored(in);
+        }
+
+        @Override
+        public ActivityMonitored[] newArray(int size) {
+            return new ActivityMonitored[size];
+        }
+    };
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeByte((byte) (isMonitored ? 1 : 0));
+        dest.writeString(activitySimpleName);
     }
 }
